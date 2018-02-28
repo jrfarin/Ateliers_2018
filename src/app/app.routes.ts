a8fede93ffc40d555977c8bd1ff620ee7e82e703
jrfarin <pepfarin@gmail.com>
2018-02-28 09:03:42 +0100

    Abril Atelier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { RecetasComponent } from './components/recetas/recetas.component';
 const APP_ROUTES: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'recetas', component: RecetasComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'recetas' }
+  { path: '**', pathMatch: 'full', redirectTo: 'main' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
